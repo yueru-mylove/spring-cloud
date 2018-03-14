3d5420024da140785a5ce70a4eb4ddc2e287e332
yueru-mylove <yueru-mylove@126.com>
2018-03-15 00:50:09 +0800

    commit for comment update

--- cloud-feign/src/main/java/com/miracle/cloud/config/CustomizedFeignConfiguration.java
+++ cloud-feign/src/main/java/com/miracle/cloud/config/CustomizedFeignConfiguration.java
@@ -4,21 +4,13 @@ import feign.Contract;
 import feign.Logger;
 import feign.Retryer;
 import org.apache.tomcat.jni.Time;
-import org.springframework.cloud.netflix.feign.AnnotatedParameterProcessor;
-import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
-import org.springframework.cloud.netflix.feign.support.SpringMvcContract;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.ConversionService;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class CustomizedFeignConfiguration {
 
     @Bean
     Logger.Level createLogLevel() {
-
         return Logger.Level.FULL;
     }
 
---
+++ cloud-ribbon-hystrix/src/main/java/com/RibbonHystrixServer.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class RibbonHystrixServer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RibbonHystrixServer.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
